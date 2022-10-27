e7ca6de7f8095c0ed8bee16b4b465f374dd15ea9
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2022-10-27 01:42:20 -0700

    (*) faster (i guess)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from quart import Quart, send_file
 from aiohttp import ClientSession
-from aiopath import AsyncPath
+from aiofiles.os import path
 import aiofiles
 
 app = Quart(__name__)
@@ -11,22 +11,20 @@ app = Quart(__name__)
 @app.route("/medals/web/<medal>")
 @app.route("/images/medals-client/<medal>")
 async def medalRequest(medal):
-    path = f"medals/osu/{medal}"
-    Path = AsyncPath(path)
+    Path = f"medals/osu/{medal}"
     # if we already have it, pull from the disk.
-    if not await Path.exists():
+    if not await path.exists(Path):
         #We don't have it saved locally, request it from osu! and save it.
         async with ClientSession() as session:
             async with session.get(f"https://s.ppy.sh/images/medals-client/{medal}") as req:
                 if req.status != 200:
                     return "Not Found"
 
-                file = await aiofiles.open(path, mode="wb")
+                file = await aiofiles.open(Path, mode="wb")
                 await file.write(await req.read())
                 await file.close()
-                return await send_file(path)
+                return await send_file(Path)
 
-
-    return await send_file(path)
+    return await send_file(Path)
 
 app.run()
