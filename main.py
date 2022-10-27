883669dcf28792a7f5bd442d4190785943944f05
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2022-10-27 01:30:42 -0700

    (*) aiopath and aiohttp

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-from os import path
 from quart import Quart, send_file
-import requests
-import shutil
+from aiohttp import ClientSession
+from aiopath import AsyncPath
+import aiofiles
 
 app = Quart(__name__)
 
 # thanks airiuwu for helping me a bit to make this faster <3
-# it's my first time using quart.
 
 @app.route("/medals/client/<medal>")
 @app.route("/medals/web/<medal>")
 @app.route("/images/medals-client/<medal>")
 async def medalRequest(medal):
+    path = f"medals/osu/{medal}"
+    Path = AsyncPath(path)
     # if we already have it, pull from the disk.
-    if not path.exists(f"medals/osu/{medal}"):
+    if not await Path.exists():
         #We don't have it saved locally, request it from osu! and save it.
-        # TODO: look into aiopath
-        req = requests.get(f"https://s.ppy.sh/images/medals-client/{medal}", stream=True)
-        if req.status_code != 200:
-            return "not found"
+        async with ClientSession() as session:
+            async with session.get(f"https://s.ppy.sh/images/medals-client/{medal}") as req:
+                if req.status != 200:
+                    return "Not Found"
 
-        with open(f"medals/osu/{medal}", "wb") as file:
-            req.raw.decode_content = True
-            shutil.copyfileobj(req.raw, file)
-            return await send_file(f"medals/osu/{medal}")
+                file = await aiofiles.open(path, mode="wb")
+                await file.write(await req.read())
+                await file.close()
+                return await send_file(path)
 
-    return await send_file(f"medals/osu/{medal}")
+
+    return await send_file(path)
 
 app.run()
