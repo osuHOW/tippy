7625155507fe6f9bc1f232c1585308dc2deaaae0
Sakuru <74860621+SakuruWasTaken@users.noreply.github.com>
2022-10-29 23:31:04 -0700

    (*) rate limit requests to osu

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from quart import Quart, send_file
 from aiohttp import ClientSession
 from aiofiles.os import path
 import aiofiles
+from ratelimit import limits, RateLimitException
 
 app = Quart(__name__)
 
-# thanks airiuwu for helping me a bit to make this faster <3
+# thanks airiuwu for helping me to make this faster <3
 
 @app.route("/medals/client/<medal>")
 @app.route("/medals/web/<medal>")
@@ -14,17 +15,30 @@ async def medalRequest(medal):
     Path = f"medals/osu/{medal}"
     # if we already have it, pull from the disk.
     if not await path.exists(Path):
-        #We don't have it saved locally, request it from osu! and save it.
-        async with ClientSession() as session:
-            async with session.get(f"https://s.ppy.sh/images/medals-client/{medal}") as req:
-                if req.status != 200:
-                    return "Not Found"
-
-                file = await aiofiles.open(Path, mode="wb")
-                await file.write(await req.read())
-                await file.close()
+        #We don't have it saved locally, request it from osu!
+        try:
+            if await requestMedalFromOsu(medal, Path):
                 return await send_file(Path)
 
+            return "not found"
+
+        except RateLimitException: 
+            return "rate limit"
+            
     return await send_file(Path)
+
+# This should be a good limit,
+# You might need to increase it if you don't already have all the medals saved.
+@limits(calls=20, period=1200)
+async def requestMedalFromOsu(medal, Path):
+    async with ClientSession() as session:
+        async with session.get(f"https://s.ppy.sh/images/medals-client/{medal}") as req:
+            if req.status != 200:
+                return False
+
+            file = await aiofiles.open(Path, mode="wb")
+            await file.write(await req.read())
+            await file.close()
+            return True
 
 app.run()
